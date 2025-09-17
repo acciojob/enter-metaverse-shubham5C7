7573574153c4f23c2f5b943fd1ad50028bbb3ecf
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 12:30:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//your JS code here. If required.
 
-    document.getElementById("enterBtn").addEventListener("click",()=>{
-      const p = document.getElementById("status");
+ document.getElementById("enterBtn").addEventListener("click", () => {
+          const statusP = document.getElementById("status");
 
-		  statusP.innerHTML = "";
-		
-      const h1 = document.createElement("h1");
+          statusP.innerHTML = "";
+
+          
+          const h1 = document.createElement("h1");
           h1.innerText = "Entered Metaverse";
+
           statusP.appendChild(h1);
-		
      });
