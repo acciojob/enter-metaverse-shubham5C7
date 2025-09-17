a93899f46f8b4bcb7d607ad0438afbcc66408eb1
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 12:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+    document.getElementById("enterBtn").addEventListener("click",()=>{
+      const p = document.getElementById("status");
+
+      const h1 = document.createElement("h1");
+      h1.innerText ="Enter the Metaverse";
+ p.replaceWith(h1);
+     })
